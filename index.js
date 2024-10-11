12ee73dcc5b8daca9f9238d4d9cf159c55643011
Matias Salazar <matias39974593@gmail.com>
2024-10-11 19:10:02 -0300

    configure el servidor en el puerto 4001 y dio una respuesta exitosa

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ import express from "express";
 //1-Configurar un puerto
 const app = express();
 
-app.set("port", process.env.PORT || 4000);
+app.set("port", process.env.PORT || 4001);
 app.listen(app.get("port"), ()=>{
     console.info("Estoy escuchando el puerto " +app.get("port") )
 })
 //2-Configurar middlewares
+//Agregar como recibir conexiones remotas
 
 //3-Configurar las rutas
+app.get("/prueba", (req, res)=>{
+    console.log("desde la funcion de prueba")
+    res.send("Desde el backend del proyecto listaTarea")
+})
