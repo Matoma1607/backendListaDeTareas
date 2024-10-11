36032f5f62b681fea911f6b297705eb2d89e69f5
Matias Salazar <matias39974593@gmail.com>
2024-10-11 18:19:30 -0300

    se agrego el puerto 4000 o una variable de entorno en la configurar principal

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hola mundo 3")
+import express from "express";
+
+//1-Configurar un puerto
+const app = express();
+
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>{
+    console.info("Estoy escuchando el puerto " +app.get("port") )
+})
+//2-Configurar middlewares
+
+//3-Configurar las rutas
