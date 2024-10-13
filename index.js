622af64c3b782ffaf8da8d4dd9ad0c68d520218c
Matias Salazar <matias39974593@gmail.com>
2024-10-12 23:19:43 -0300

    agregamos middlewares para configurar archivos estaticos

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import express from "express";
 import cors from "cors";
-import morgan from "morgan"
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //1-Configurar un puerto
 const app = express();
 
 app.set("port", process.env.PORT || 4001);
-app.listen(app.get("port"), ()=>{
-    console.info("Estoy escuchando el puerto " +app.get("port") )
-})
+app.listen(app.get("port"), () => {
+  console.info("Estoy escuchando el puerto " + app.get("port"));
+});
 //2-Configurar middlewares
-app.use(cors()); 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-
-//Agregar como recibir conexiones remotas
-
+app.use(express.urlencoded({ extended: true }));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, "/public")));
 //3-Configurar las rutas
-app.get("/prueba", (req, res)=>{
-    console.log("desde la funcion de prueba")
-    res.send("Desde el backend del proyecto listaTarea")
-})
+app.get("/prueba", (req, res) => {
+  console.log("desde la funcion de prueba");
+  res.send("Desde el backend del proyecto listaTarea");
+});
