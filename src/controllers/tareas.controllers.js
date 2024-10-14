73f433afc88948e4bf3d0f94544a3479573baf70
Matias Salazar <matias39974593@gmail.com>
2024-10-14 00:20:23 -0300

    se agrego logica para crear una tarea y se contesto con error cuando la tarea no pudo ser generado

--- src/controllers/tareas.controllers.js
+++ src/controllers/tareas.controllers.js
@@ -1,3 +1,21 @@
+import Tarea from "../database/model/tarea.js";
+
 export const leerPrueba = (req, res) => {
   res.send("Desde el backend del proyecto listaTarea");
 };
+export const crearTarea = async (req, res) => {
+    try {
+        const tareaNueva = new Tarea(req.body);
+        await tareaNueva.save();
+        res.status(201).json({
+            mensaje:"El producto fue creado"
+        })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({
+            mensaje:"Ocurrio un error"
+        })
+    }
+  };
+  
+  
--- src/routes/tareas.routes.js
+++ src/routes/tareas.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { leerPrueba } from "../controllers/tareas.controllers.js";
+import { crearTarea, leerPrueba } from "../controllers/tareas.controllers.js";
 
 const router = Router();
 router.route("/prueba").get(leerPrueba);
+router.route("/tareas").post(crearTarea);
 
 export default router;
